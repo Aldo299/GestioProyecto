41176297f04067fd89902e8cfedbb2ba7b92e2dc
01OscarRam <oscarramirezalba012003@gmail.com>
2024-12-03 11:08:09 -0600

    Muestra solo el usuario logueado

--- example-app/app/Http/Controllers/MenuController.php
+++ example-app/app/Http/Controllers/MenuController.php
@@ -3,19 +3,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Bicicleta;  // Importa el modelo Bicicleta
+use Illuminate\Support\Facades\Auth;  // Importa la clase Auth para obtener el usuario autenticado
 
 class MenuController extends Controller
 {
     /**
-     * Mostrar la vista del menú y todas las bicicletas.
+     * Mostrar la vista del menú y las bicicletas del usuario autenticado.
      */
     public function index()
     {
-        // Obtener todas las bicicletas desde la base de datos
-        $bicicletas = Bicicleta::all();
+        // Obtener el no_control del usuario autenticado
+        $noControl = Auth::user()->no_control;
+
+        // Obtener todas las bicicletas del usuario autenticado
+        $bicicletas = Bicicleta::where('no_control', $noControl)->get();
 
         // Retornar la vista 'menu' con la variable $bicicletas
         return view('menu', compact('bicicletas'));
     }
 }
+
 
